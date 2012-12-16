738351d7572f7b214bfa369a60c6be19e36f8065
Gary Lucas <lucas.gary@gmail.com>
2012-12-16 14:46:18 -0800

    Service Locator verifies that enum input value is returned as matching type

---
+++ AccountTransferKata/AccountTransferKataTests/Utils.Tests.Unit/ServiceLocatorTests.h
@@ -0,0 +1,13 @@
+//
+//  ServiceLocatorTests.h
+//  AccountTransferKata
+//
+//  Created by gary lucas on 12/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ServiceLocatorTests : SenTestCase
+
+@end
---
+++ AccountTransferKata/AccountTransferKataTests/Utils.Tests.Unit/ServiceLocatorTests.m
@@ -0,0 +1,14 @@
+#import "ServiceLocatorTests.h"
+#import "PresenterTypeEnum.h"
+#import "ServiceLocator.h"
+#import "AccountTransferPresenter.h"
+
+@implementation ServiceLocatorTests
+-(void) testGivenSUTWhenResolveMethodIsPassedEnumerationThanMatchingTypeIsInstantiatedAndReturned {
+    PresenterTypeEnum presenterTypeEnum = AccountTransfer;
+    id accountTransferPresenter = [ServiceLocator resolve:presenterTypeEnum];
+
+    STAssertTrue([accountTransferPresenter isKindOfClass:[AccountTransferPresenter class]], @"The expected Return type was not returned");
+
+}
+@end
